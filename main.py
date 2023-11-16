2e69977e10e0c289832d970c89b01188ab78a9c6
Medina Jimenez Luis Herney <lhmedina@fanalca.com>
2023-11-16 09:20:16 -0500

    API subir imagen

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, File, UploadFile
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Hola Mundo"}
+
+
+def save_image(file: bytes, folder_path: str):
+    with open(os.path.join(folder_path,"image.jpg"), "wb") as f:
+        f.write(file)
+
+@app.post("/files/")
+async def create_file(file: Annotated[bytes, File()]):
+    folder_path = "./Img"
+    save_image(file, folder_path, file.filename)
+    return {"file_size": len(file)}
