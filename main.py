c9236dd1d0da7cc9fe666a8829bb44392f5db9fe
1treu1 <luis.medina@utp.edu.co>
2023-12-28 02:45:36 +0000

    cargando imagen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from typing import Annotated
 from fastapi import FastAPI, File, UploadFile
+from google.cloud import storage 
 import os
 
 app = FastAPI()
@@ -10,12 +11,24 @@ def home():
     return {"message": "Hola Mundo"}
 
 
-def save_image(file: bytes, folder_path: str):
-    with open(os.path.join(folder_path,"image.jpg"), "wb") as f:
-        f.write(file)
+def save_image(file: bytes, blob_name: str, bucket_name: str):
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    try:
+        with blob.open("wb") as f:
+            f.write(file)
+            print("Guardado")
+    except Exception as e:
+        print(e)
+    #with open(os.path.join(folder_path,"image.jpg"), "wb") as f:
+    
 
 @app.post("/files/")
 async def create_file(file: Annotated[bytes, File()]):
-    folder_path = "./Img"
-    save_image(file, folder_path, file.filename)
+    blob_name = "img.jpeg"
+    bucket_name = "documentos-ocr-lucho"
+    save_image(file, blob_name, bucket_name)
     return {"file_size": len(file)}
